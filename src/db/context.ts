9607c515ca75090486d5068180bb661b088562ac
davi g nascimento <davign20@outlook.com>
2022-06-16 20:45:58 -0300

    Formatting

---
+++ src/db/context.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import Role from '../models/Role';
+import User from '../models/User';
+
+const DbContext = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '123456',
+    database: 'turma836',
+    synchronize: false,
+    logging: false,
+    entities: [User, Role],
+    subscribers: [],
+    migrations: ['./src/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+});
+
+export default DbContext;
